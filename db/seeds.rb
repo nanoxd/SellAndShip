381904b3ff2be67621dbecbb8269f6babe2df79b
Fernando Paredes <nano@fdp.io>
2013-08-08 14:36:29 -0700

    Add Products MVC and testing seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.delete_all
+Product.create!(title: 'Hammer',
+  description:
+    %{<p>
+        A hammer is a tool meant to deliver an impact to an object. The most common uses for hammers are to drive nails, fit parts, forge metal and break apart objects
+      </p>},
+  image_url:   'hammer.jpg',
+  price: 19.99)
+# . . .
+Product.create!(title: 'Phillips Screwdriver',
+  description:
+    %{<p>
+        A screwdriver screws on screws :).
+      </p>},
+  image_url: 'screwdriver.jpg',
+  price: 9.95)
+# . . .
+
+Product.create!(title: 'Drill',
+  description:
+    %{<p>
+         A drill is a tool fitted with a cutting tool attachment or driving tool attachment, usually a drill bit or driver bit, used for drilling holes in various materials or fastening various materials together with the use of fasteners.
+      </p>},
+  image_url: 'drill.jpg',
+  price: 34.95)
