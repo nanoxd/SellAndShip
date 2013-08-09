cb2ea12c08631a7ce9f8c3217dcbecd931d87760
Fernando Paredes <nano@fdp.io>
2013-08-08 20:58:07 -0700

    Add validation to Product model

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,57 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :products
+
+  def new_product(image_url)
+    Product.new(title: "Harry Potter and the Chamber of Secrets.",
+                description: "A fantastic book on a wizard and his snake.",
+                price: 1,
+                image_url: image_url)
+  end
+
+  test "product attributes must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
+  end
+
+  test "price is positive" do
+    product = Product.new(title: "Harry Potter and the Camber of Secrets",
+                          description: "A fantastic book on a wizard and his snake.",
+                          image_url: "hp.jpg")
+    product.price = -1
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"],
+      product.errors[:price]
+
+    product.price = 0
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+
+    product.price = 1
+    assert product.valid?, product.errors[:title]
+  end
+
+
+  test "image url should work" do
+    good_images = %w{ harry.gif harry.jpg harry.png HARRY.JPG HARRY.Jpg http://google.com/i/harry.gif }
+
+    good_images.each do |image|
+      assert new_product(image).valid?, "#{image} should be valid."
+    end
+  end
+
+  test "image url should not work" do
+    bad_images = %w{ harry.doc harry.gif.html harry.svg }
+
+    bad_images.each do |image|
+      assert new_product(image).invalid?, "#{image} shoudn't be valid"
+    end
+    
+  end
+
 end
