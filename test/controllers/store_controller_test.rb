3446e5c11d3870eea99266af90fa2725b7f94334
Fernando Paredes <nano@fdp.io>
2013-08-13 15:08:00 -0700

    Add Ajax for image addition of items

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -6,4 +6,10 @@ class StoreControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "markup needed for store.js.coffee is in place" do
+    get :index
+    assert_select '.store .entry > img', 2
+    assert_select '.entry input[type=submit]', 2
+  end
+
 end
