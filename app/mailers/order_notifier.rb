13b13465f9461734385e9b92c808f952ae7f7e2a
Fernando Paredes <nano@fdp.io>
2013-08-14 14:08:01 -0700

    Add integration test and Ship Date to Orders table

--- app/mailers/order_notifier.rb
+++ app/mailers/order_notifier.rb
@@ -1,5 +1,5 @@
 class OrderNotifier < ActionMailer::Base
-  default from: "Fernado Paredes <from@example.com>"
+  default from: "Fernando Paredes <from@example.com>"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130813221023) do
+ActiveRecord::Schema.define(version: 20130814210728) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20130813221023) do
     t.string   "pay_type"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "ship_date"
   end
 
   create_table "products", force: true do |t|
