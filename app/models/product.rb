65c5c89d4f55eb71ca9bf9074183e5ee63d3e05a
Fernando Paredes <nano@fdp.io>
2013-08-13 19:09:33 -0700

    Add atom feed for products and complete checkout process

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ActiveRecord::Base
   has_many :line_items
+  has_many :orders, through: :line_items
   before_destroy :ensure_not_referenced_by_any_line_item
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
@@ -8,6 +9,7 @@ class Product < ActiveRecord::Base
     with: %r{\.(gif|jpg|png)\Z}i,
     message: 'must be a URL for GIF, JPG, or PNG image.'
   }
+  validates :image_url, uniqueness: true
 
   def self.latest
     Product.order(:updated_at).last
